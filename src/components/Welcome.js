94ad08d771f2cdc41618a5b4b8c2cb42c2c7c0a4
amjad <amjadsam272@gmail.com>
2021-03-11 23:23:24 +0530

    change

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,10 +2,10 @@
 import React from "react";
 export default function Welcome(props)
 {
-    return(
-        <div>
-            <h1>"{props.name} !"</h1>
-            <h2>"Welcome to Newton School."</h2>
-        </div>
-    );
+  return(
+    <>
+<h1>"{props.name} !"</h1>
+<h2>"Welcome to Newton School."</h2>
+  </>
+  );
 }
